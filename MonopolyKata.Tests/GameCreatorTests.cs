378811f5cf26fd69818cc8cc550969a3c598561c
Bryan Prince <bprince@ccs-inc.com>
2014-01-24 16:09:55 -0500

    Added a test for the GameBuilder and its ability to set players

---
+++ MonopolyKata.Tests/GameCreatorTests.cs
@@ -0,0 +1,26 @@
+﻿using MonopolyKata.Core;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace MonopolyKata.Tests
+{
+    [TestFixture]
+    public class GameCreatorTests
+    {
+        [Test]
+        public void CanCreate()
+        {
+            var gameBuilder = new GameBuilder();
+        }
+
+        [Test]
+        public void SetPlayers_ShouldAddPlayers()
+        {
+            var gameBuilder = new GameBuilder();
+            var players = new List<string>() { "Horse", "Car"};
+
+            gameBuilder.SetPlayers(players);
+            Assert.That(gameBuilder.GetGame().Players, Is.EquivalentTo(players));
+        }
+    }
+}
