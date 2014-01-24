131a69c8c385b714a108b1c580f45267d851e0ed
Bryan Prince <bprince@ccs-inc.com>
2014-01-24 14:42:36 -0500

    Added mover class and first tests

---
+++ MonopolyKata.Tests/MoverTests.cs
@@ -0,0 +1,33 @@
+﻿using MonopolyKata.Core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonopolyKata.Tests
+{
+    [TestFixture]
+    public class MoverTests
+    {
+        [Test]
+        public void CanCreate()
+        {
+            var mover = new Mover();
+
+            Assert.That(mover, Is.Not.Null);
+        }
+
+        [Test]
+        public void Move_ShouldIncreaseThePlayerLocationByTheSpecifiedAmount()
+        {
+            var mover = new Mover();
+            mover.SetPlayerLocation("Prinsy", "Xero");
+
+            mover.Move("Prinsy", 7);
+
+            Assert.That(mover.GetPlayerLocation("Prinsy", Is.EqualTo("Seven)")));
+        }
+    }
+}
